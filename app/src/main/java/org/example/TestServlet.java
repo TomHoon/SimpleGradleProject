226cf85fc6f7204770f870a99d2b1aeb2c67994c
tomhoon <gnsdl9108@naver.com>
2025-04-26 09:19:06 +0900

    first commit

---
+++ app/src/main/java/org/example/TestServlet.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/test")
+public class TestServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().print("GET TEST");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("testkey", "Test val");
+        req.getRequestDispatcher("test.jsp").forward(req, resp);
+    }
+}
